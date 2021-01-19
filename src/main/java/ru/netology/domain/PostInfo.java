e1a6db724b5e0d5054fbc05a09720194db9493e6
San4ez63 <npsamara@bk.ru>
2021-01-19 20:38:32 +0400

    Initial commit

---
+++ src/main/java/ru/netology/domain/PostInfo.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+public class PostInfo {
+    private int id;
+    private String headerPostTab;
+    private String postAuthor;
+    private int postDate;
+    private String menuWrap;
+    private String postText;
+    private String postImgUrl;
+    private int footerCountButton;
+    private String buttonLike;
+    private String buttonComment;
+    private String buttonViews;
+    private String postReplies;
+}
+
