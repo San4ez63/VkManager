feccbdc13eeb485b762dadafe42502bf832433e7
San4ez63 <npsamara@bk.ru>
2021-01-21 20:33:45 +0400

    added 2Home Work

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.PostInfo;
+
+    public class PostManager {
+        private PostInfo[] postInfos;
+        public PostInfo[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset){
+            return null;
+        }
+
+        public void delete(int ownerId, int postId){
+
+        }
+}
